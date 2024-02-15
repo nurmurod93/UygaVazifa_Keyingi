1e369c4cd0859a742586f4bd25904f6e3573cd6a
Humoyun Ochilov Ravshan o'g'li <43071439+dotNet-developer1995@users.noreply.github.com>
2024-02-15 12:05:53 +0500

    Add project files.

---
+++ Dars_TryCatch_Delegate_Keyingi/Form1.cs
@@ -0,0 +1,11 @@
+namespace Dars_TryCatch_Delegate_Keyingi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
